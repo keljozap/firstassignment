d6bd2e5963f2830d8513afda1fe8aaaf90b82e8c
keljozap <kelly_zapata27@hotmail.com>
2019-05-12 19:43:31 -0500

    first commit

---
+++ courses.js
@@ -0,0 +1,30 @@
+let courses=[{
+	id:001,
+	name:'Quantum Physics',
+	duration:'3 months',
+	price:'20 dollars'
+},
+{
+	id:002,
+	name:'Telecommunication Networks',
+	duration:'2 months',
+	price:'25 dollars'
+},
+{
+	id:003,
+	name:'Advanced English',
+	duration:'3 months',
+	price:'30 dollars'
+},
+{
+	id:004,
+	name:'Electric Machines',
+	duration:'3 months',
+	price:'20 dollars'
+},
+{
+	id:005,
+	name:'Advanced Programming',
+	duration:'3 months',
+	price:'50 dollars'
+}];
